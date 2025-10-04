36d6d243ecef0e211e656f60e2169021690621dc
kingsbalfx <shafiuabdullahi.sa3@gmail.com>
2025-10-04 20:15:38 +0100

    Update pricing.js

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,7 +1,5 @@
 // pages/pricing.js
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function Pricing() {
   const plans = [
