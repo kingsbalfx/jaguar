8e5ab1d8ee5aeeaadca8bdde61f4dbec68226491
kingsbalfx <shafiuabdullahi.sa3@gmail.com>
2025-10-04 18:39:49 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const AD_EXCLUDE_PATHS = [
   "/login",
