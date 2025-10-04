d55153d9857df8a3cd811c29f49c096a18220026
kingsbalfx <shafiuabdullahi.sa3@gmail.com>
2025-10-03 21:09:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import AdSense from "../components/AdSense";
 
 export default function Home() {
