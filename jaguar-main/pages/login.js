5847823aec0a829cde072dfeb0a8528a1c801375
Kingsbal Dev <dev@kingsbal.com>
2026-02-21 22:39:14 +0100

    Fix login client-only import

--- jaguar-main/pages/login.js
+++ jaguar-main/pages/login.js
@@ -1,7 +1,10 @@
 // pages/login.js
 import dynamic from "next/dynamic";
 
-const LoginPage = dynamic(() => import("../components/LoginPage"), { ssr: false });
+const LoginPage = dynamic(
+  () => import("../components/LoginPage").then((mod) => mod.default),
+  { ssr: false, loading: () => <div className="min-h-[calc(100vh-160px)]" /> }
+);
 
 export default function Login() {
   return <LoginPage />;
